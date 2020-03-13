2db4a3ba58616636ac7984b5a1c9189ba9b4cd2b
lichunshan <2375974889@qq.com>
2020-03-13 10:27:13 +0800

    删除src参数

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <pdf-preview src="http://dl.chasing-innovation.com/dory%E5%BF%AB%E9%80%9F%E5%85%A5%E9%97%A8%E6%8C%87%E5%8D%97.pdf"></pdf-preview>
+    <pdf-preview src=""></pdf-preview>
   </div>
 </template>
 
